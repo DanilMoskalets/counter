bfb47e070383d1f349e2f2f826fe582f31e29999
Данил <dan.moskalets@gmail.com>
2022-05-23 22:36:51 +0300

    Commit to Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent, ChangeEventHandler, FormEvent, useState} from 'react';
+import s from './App.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+type initialStateType = {
+    counter: number,
+    maxValue: number
+    startValue: number
+    settings: boolean
+    error: boolean
 }
 
-export default App;
+
+const initialState = {
+    counter: 0,
+    maxValue: 1,
+    startValue: 0,
+    settings: true,
+    error: false
+}
+
+
+
+export const App = () => {
+    const [value, setValue] = useState<initialStateType>(initialState)
+    const {counter, maxValue, startValue, settings, error} = value
+
+    const incrementCounter = () => {
+        if(counter <= maxValue ) {
+            setValue({...value, counter:  counter + 1})
+        }
+    }
+
+    const resetCounter = () => {
+        setValue({...value, counter: startValue, settings: true})
+    }
+
+    const handleOnChangeInputMax = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue({...value, maxValue: Number(e.currentTarget.value)})
+    }
+
+    const handleOnChangeInputStart = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue({...value, startValue: Number(e.currentTarget.value)})
+
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setValue({...value, counter: startValue, settings: false})
+
+    }
+
+
+
+    const disabledInc = counter === maxValue || settings  ? true : false
+    const disabledReset =  counter ===  startValue ? true : false
+    return (
+        <div className={s.app}>
+            <div>
+                <div className={s.num}>{ counter}</div>
+                <button disabled={disabledInc} onClick={incrementCounter} >inc</button>
+                <button disabled={disabledReset} onClick={resetCounter}>reset</button>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type='number' value={ maxValue}   onChange={ handleOnChangeInputMax}/>
+                    <input type='number'  value={ startValue} onChange={handleOnChangeInputStart}/>
+                    <button  type='submit'> Set </button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
